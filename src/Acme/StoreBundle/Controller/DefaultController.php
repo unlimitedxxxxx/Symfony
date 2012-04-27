4e064b3a6860eda9f993faaf984c16ac07306c56
tomoki_shibahara <tomoki_shibahara@Suncorp1381.sun.center>
2012-04-27 18:13:05 +0900

    ORM compleate!

--- src/Acme/StoreBundle/Controller/DefaultController.php
+++ src/Acme/StoreBundle/Controller/DefaultController.php
@@ -3,13 +3,99 @@
 namespace Acme\StoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Acme\StoreBundle\Entity\Category;
+use Acme\StoreBundle\Entity\Product;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     
-    public function indexAction($name)
+   public function indexAction($name)
+   {
+      return $this->render('AcmeStoreBundle:Default:index.html.twig', array('name' => $name));
+   }
+    
+   public function createAction()
+   {
+      $product = new Product();
+      $product->setName('A Foo Bar');
+      $product->setPrice('19.99');
+      $product->setDescription('Lorem ipsum dolor');
+
+      $em = $this->getDoctrine()->getEntityManager();
+      $em->persist($product);
+      $em->flush();
+
+      return new Response('Created product id '.$product->getId());
+   }
+   
+   public function showAction($id)
+   {
+       $product = new Product();
+      $product = $this->getDoctrine()
+         ->getRepository('AcmeStoreBundle:Product')
+         ->find($id);
+
+      if (!$product) {
+         throw $this->createNotFoundException('No product found for id '.$id);
+      }
+
+      $category = new Category();
+      $category = $product->getCategory();
+      
+      $cate = $category->getName();
+      return new Response('Show-'.$cate);
+      
+   }
+
+   public function updateAction($id)
+   {
+      $em = $this->getDoctrine()->getEntityManager();
+      $product = $em->getRepository('AcmeStoreBundle:Product')->find($id);
+
+      if (!$product) {
+         throw $this->createNotFoundException('No product found for id '.$id);
+      }
+
+      $product->setName('New product name!');
+      $em->flush();
+
+      return $this->redirect($this->generateUrl('homepage'));
+   }
+   
+   public function createProductAction()
     {
-        return $this->render('AcmeStoreBundle:Default:index.html.twig', array('name' => $name));
+        $category = new Category();
+        $category->setName('Main Products');
+
+        $product = new Product();
+        $product->setName('Foo');
+        $product->setPrice(19.99);
+        $product->setDescription('test');
+        
+        // この商品をカテゴリに関連付ける
+        $product->setCategory($category);
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $em->persist($category);
+        $em->persist($product);
+        $em->flush();
+
+        return new Response(
+            'Created product id: '.$product->getId().' and category id: '.$category->getId()
+        );
+    }
+    
+    public function showProductAction($id)
+    {
+        $category = $this->getDoctrine()
+            ->getRepository('AcmeStoreBundle:Category')
+            ->find($id);
+
+        $products = $category->getProducts();
+
+        // ...
+        return new Response('showProduct');
+
     }
 }
---
+++ src/Acme/StoreBundle/Entity/Category.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Acme\StoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+/**
+ * Acme\StoreBundle\Entity\Category
+ *
+ * @ORM\Table(name="Cagegory")
+ * @ORM\Entity
+ */
+class Category
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    
+    /**
+     * @ORM\OneToMany(targetEntity="Product", mappedBy="category")
+     */
+    protected $products;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
+
+    /**
+     * Add products
+     *
+     * @param Acme\StoreBundle\Entity\Product $products
+     */
+    public function addProduct(\Acme\StoreBundle\Entity\Product $products)
+    {
+        $this->products[] = $products;
+    }
+
+    /**
+     * Get products
+     *
+     * @return Doctrine\Common\Collections\Collection 
+     */
+    public function getProducts()
+    {
+        return $this->products;
+    }
+}
